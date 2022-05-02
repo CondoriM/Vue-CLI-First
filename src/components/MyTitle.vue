51d3c33b7db6dd6651804eb54fc67d57300f5bbc
CondoriM <condoria.miguel@gmail.com>
2022-05-02 16:19:28 +0200

    creazione MyTilte

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>ciao Boolean</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
